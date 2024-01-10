cdd04a9e315a0b5c1afa4a14a4cdd07d9c760eba
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-10 11:20:13 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const student={
+	name : 'taj'
+}
+Object.prototype.getKeys = function(){
+	return Object.keys(this) ;
+}
+let result  = student.getKeys();
+console.log(result)
